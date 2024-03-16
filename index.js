d918a59c6cbe42cc104b23547c4b54328fd09329
Prasad Kute <prasadkute0708@gmail.com>
2024-03-17 04:21:32 +0530

    npm setup done

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "india@11",
+  port: 5432,
+});
+
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// // Database functions
+// async function addTaskDB(newtitle) {
+//   if (newtitle === null) {
+//     console.log("Can't insert null value");
+//   } else {
+//     await db.query("INSERT INTO items (title) VALUES ($1)", [newtitle]);
+//   }
+// }
+
+app.get("/", async (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
