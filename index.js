33faab5906e565fcb104d114b11e2eed3997c520
durvankur <durvankur.rasal48@gmail.com>
2024-03-17 05:31:32 +0530

    log in temp done

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
-import pg from "pg";
+// import pg from "pg";
 
 const app = express();
 const port = 3000;
 
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "permalist",
-  password: "india@11",
-  port: 5432,
-});
+// const db = new pg.Client({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "permalist",
+//   password: "@drr96",
+//   port: 5432,
+// });
 
-db.connect();
+// db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
